a093ef4ddc75c326294cb82175756b4e40c436a5
eridev14 <eridev14hc@gmail.com>
2024-06-27 23:49:59 -0400

    add module variables

---
+++ src/variables.js
@@ -0,0 +1,22 @@
+export let listMonth = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+export let listArray = [
+  {
+    id: 0,
+    name: "vida",
+    // tasksArray: [],
+  },
+];
